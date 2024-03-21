4234c38566cf3874736af22742c0e83707225c6f
heloultramare <ultramareh@gmail.com>
2024-03-21 14:30:45 -0300

    Incial

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import { Image, Text, View, StyleSheet } from "react-native";
 
-export default function Home()
-{
-    return(
+export default function Home() {
+    return (
         <View>
-            <Text>Home</Text>
+            <Image style={css.imagem} source={require('../assets/FundoHome.png')} />
         </View>
     )
 }
+
+const css = StyleSheet.create({
+    imagem: {
+        alignItems: "center",
+        justifyContent: "center",
+        resizeMode: "contain",
+        width: 390,
+        height: 660
+
+    }
+})
